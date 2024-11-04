787d4c5e88e1920b9b9449146fa0c1fc836c96fa
tmosimanyana <mos23018@byui.edu>
2024-11-04 18:06:53 +0200

    files updated

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -7,19 +7,22 @@ public Person GetNextPerson()
     
     Person person = _people.Dequeue();
 
-    // Handle turns: decrement if more than 0; if 0 or less, enqueue again
+    // Handle turns: decrement if more than 0; enqueue again if turns are infinite (0 or less)
     if (person.Turns > 0)
     {
-        person.Turns -= 1;
-        if (person.Turns >= 0) // Enqueue again if still has turns left
+        person.Turns -= 1; // Decrement the turns
+
+        // Enqueue again if they still have turns left after decrementing
+        if (person.Turns > 0) 
         {
             _people.Enqueue(person);
         }
     }
+    // If turns is 0 or less, they stay in the queue forever.
     else
     {
-        // If turns is 0 or less, they stay in the queue forever.
-        _people.Enqueue(person);
+        // In this case, no action is needed since they will stay in the queue
+        _people.Enqueue(person); // Optionally, you could remove this line if they should not be enqueued again
     }
 
     return person;
