4563f9b461c2a6008da5b0109828a80b01796e0f
tmosimanyana <mos23018@byui.edu>
2024-11-06 12:58:10 +0200

    files updated

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -8,21 +8,35 @@ public class PersonQueue
     public int Length => _queue.Count;
 
     /// <summary>
-    /// Add a person to the queue
+    /// Add a person to the queue.
     /// </summary>
-    /// <param name="person">The person to add</param>
+    /// <param name="person">The person to add.</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Add(person); // Add to the end of the list for FIFO order
     }
 
+    /// <summary>
+    /// Remove and return the person at the front of the queue.
+    /// </summary>
+    /// <returns>The person at the front of the queue.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when trying to dequeue from an empty queue.</exception>
     public Person Dequeue()
     {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
         var person = _queue[0];
         _queue.RemoveAt(0);
         return person;
     }
 
+    /// <summary>
+    /// Checks if the queue is empty.
+    /// </summary>
+    /// <returns>True if the queue is empty; otherwise, false.</returns>
     public bool IsEmpty()
     {
         return Length == 0;
