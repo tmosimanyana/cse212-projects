b5b6c5817befb94592c8e8e52640c968337ad900
tmosimanyana <mos23018@byui.edu>
2024-11-06 13:34:05 +0200

    commited changes

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,73 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-
-public class PriorityQueue
+﻿public string Dequeue()
 {
-    private List<PriorityItem> _queue = new();
-
-    /// <summary>
-    /// Add a new value to the queue with an associated priority.  
-    /// The node is always added to the back of the queue regardless of priority.
-    /// </summary>
-    /// <param name="value">The value</param>
-    /// <param name="priority">The priority</param>
-    public void Enqueue(string value, int priority)
+    if (_queue.Count == 0) // Check if the queue is empty
     {
-        var newNode = new PriorityItem(value, priority);
-        _queue.Add(newNode);
+        throw new InvalidOperationException("The queue is empty.");
     }
 
-    /// <summary>
-    /// Removes and returns the item with the highest priority.
-    /// If multiple items have the same highest priority, the item closest to the front is removed.
-    /// </summary>
-    /// <returns>The value of the item with the highest priority.</returns>
-    public string Dequeue()
+    // Find the index of the item with the highest priority (FIFO for items with the same priority)
+    int highPriorityIndex = 0;
+    for (int index = 1; index < _queue.Count; index++)
     {
-        if (_queue.Count == 0) // Check if the queue is empty
+        // Prioritize items with higher priority, retain first occurrence in case of tie
+        if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
         {
-            throw new InvalidOperationException("The queue is empty.");
+            highPriorityIndex = index;
         }
-
-        // Find the index of the item with the highest priority to remove
-        int highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count; index++)
-        {
-            // Prioritize items with higher priority or, if equal, those closer to the front (FIFO)
-            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
-            {
-                highPriorityIndex = index;
-            }
-        }
-
-        // Retrieve, remove, and return the item with the highest priority
-        var value = _queue[highPriorityIndex].Value;
-        _queue.RemoveAt(highPriorityIndex);
-        return value;
     }
 
-    // Returns the current length of the queue (for testing purposes)
-    public int Length => _queue.Count;
-
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
-    }
-}
-
-internal class PriorityItem
-{
-    internal string Value { get; set; }
-    internal int Priority { get; set; }
-
-    internal PriorityItem(string value, int priority)
-    {
-        Value = value;
-        Priority = priority;
-    }
-
-    public override string ToString()
-    {
-        return $"{Value} (Pri:{Priority})";
-    }
+    // Retrieve, remove, and return the item with the highest priority
+    var value = _queue[highPriorityIndex].Value;
+    _queue.RemoveAt(highPriorityIndex);
+    return value;
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,17 +1,4 @@
-using System;
-using System.Collections.Generic;
-
-public class Person
-{
-    public string Name { get; set; }
-    public int Turns { get; set; }
-
-    public Person(string name, int turns)
-    {
-        Name = name;
-        Turns = turns;
-    }
-}
+using System.Diagnostics;
 
 public class TakingTurnsQueue
 {
@@ -22,36 +9,39 @@ public class TakingTurnsQueue
         queue = new Queue<Person>();
     }
 
-    // Adds a person to the queue with a given name and turn count.
     public void AddPerson(string name, int turns)
     {
         var person = new Person(name, turns);
+        Debug.WriteLine($"Added {person.Name} with {person.Turns} turns.");
         queue.Enqueue(person);
     }
 
-    // Returns the next person in line, re-adding them if they have remaining turns.
     public Person GetNextPerson()
     {
         if (queue.Count == 0)
             throw new InvalidOperationException("No one in the queue.");
 
         var person = queue.Dequeue();
+        Debug.WriteLine($"Processing {person.Name} with {person.Turns} turns.");
 
-        // Handle infinite turns (<= 0 means infinite).
         if (person.Turns <= 0)
         {
-            queue.Enqueue(person); // Re-add person with infinite turns to the end of the queue
+            queue.Enqueue(person);
+            Debug.WriteLine($"{person.Name} has infinite turns, re-added to queue.");
         }
         else if (person.Turns > 1)
         {
-            person.Turns--; // Decrease turns and re-add if more turns are left
+            person.Turns--;
             queue.Enqueue(person);
+            Debug.WriteLine($"{person.Name} has {person.Turns} turns remaining, re-added to queue.");
         }
-        // If Turns == 1, do not re-add person, as they have no turns left after this
+        else
+        {
+            Debug.WriteLine($"{person.Name} has no remaining turns, not re-added.");
+        }
 
         return person;
     }
 
-    // Property to get the current length of the queue.
     public int Length => queue.Count;
 }
