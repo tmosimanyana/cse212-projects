3e969b65c0d35ec6a8903773923d9d8407291a18
tmosimanyana <mos23018@byui.edu>
2024-11-25 23:14:16 +0200

    files updated

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,58 +1,44 @@
 using System;
 using System.Collections.Generic;
 
-public class Person
-{
-    public string Name { get; set; }
-    public int Turns { get; set; }
-
-    public Person(string name, int turns)
-    {
-        Name = name;
-        Turns = turns;
-    }
-
-    public override string ToString()
-    {
-        return Turns <= 0 ? $"({Name}:Forever)" : $"({Name}:{Turns})";
-    }
-}
-
 public class TakingTurnsQueue
 {
-    private Queue<Person> queue;
-
-    public TakingTurnsQueue()
+    private class Person
     {
-        queue = new Queue<Person>();
+        public string Name { get; }
+        public int Turns { get; set; }
+
+        public Person(string name, int turns)
+        {
+            Name = name;
+            Turns = turns;
+        }
     }
+
+    private readonly Queue<Person> _queue = new();
 
     public void AddPerson(string name, int turns)
     {
-        queue.Enqueue(new Person(name, turns));
+        _queue.Enqueue(new Person(name, turns));
     }
 
-    public Person GetNextPerson()
+    public string GetNextPerson()
     {
-        if (queue.Count == 0)
+        if (_queue.Count == 0)
             throw new InvalidOperationException("The queue is empty.");
 
-        var person = queue.Dequeue();
+        var person = _queue.Dequeue();
 
-        // If they still have turns or infinite turns, re-enqueue them.
         if (person.Turns > 0)
         {
             person.Turns--;
-            queue.Enqueue(person);
+            _queue.Enqueue(person);
         }
-        else if (person.Turns <= 0)
+        else if (person.Turns <= 0) // Infinite turns
         {
-            // They have infinite turns, re-enqueue without modifying turns.
-            queue.Enqueue(person);
+            _queue.Enqueue(person);
         }
 
-        return person;
+        return person.Name;
     }
-
-    public int Length => queue.Count;
 }
