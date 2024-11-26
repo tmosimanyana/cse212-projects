1a90ffec2b1b84440b21644f2047cfae0f8ae13d
tmosimanyana <mos23018@byui.edu>
2024-11-26 17:14:04 +0200

    files updated

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -1,93 +1,11 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-[TestClass]
-public class TakingTurnsQueue_Tests
+public class Person
 {
-    [TestMethod]
-    // Test Scenario: Run until queue is empty.
-    // Expected result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    public void TestTakingTurnsQueue_FiniteRepetition()
+    public string Name { get; }
+    public int Turns { get; }
+
+    public Person(string name, int turns)
     {
-        var bob = new Person("Bob", 2);
-        var tim = new Person("Tim", 5);
-        var sue = new Person("Sue", 3);
-
-        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, sue, tim, tim };
-
-        var players = new TakingTurnsQueue();
-        players.AddPerson(bob.Name, bob.Turns);
-        players.AddPerson(tim.Name, tim.Turns);
-        players.AddPerson(sue.Name, sue.Turns);
-
-        int i = 0;
-        while (players.Length > 0)
-        {
-            Assert.AreEqual(expectedResult[i].Name, players.GetNextPerson().Name);
-            i++;
-        }
-    }
-
-    [TestMethod]
-    // Test Scenario: Adding George midway through the queue.
-    // Expected result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    public void TestTakingTurnsQueue_AddPlayerMidway()
-    {
-        var bob = new Person("Bob", 2);
-        var tim = new Person("Tim", 5);
-        var sue = new Person("Sue", 3);
-        var george = new Person("George", 3);
-
-        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, george, sue, tim, george, tim, george };
-
-        var players = new TakingTurnsQueue();
-        players.AddPerson(bob.Name, bob.Turns);
-        players.AddPerson(tim.Name, tim.Turns);
-        players.AddPerson(sue.Name, sue.Turns);
-
-        for (int i = 0; i < 5; i++)
-        {
-            Assert.AreEqual(expectedResult[i].Name, players.GetNextPerson().Name);
-        }
-
-        players.AddPerson("George", 3);
-
-        int j = 5;
-        while (players.Length > 0)
-        {
-            Assert.AreEqual(expectedResult[j].Name, players.GetNextPerson().Name);
-            j++;
-        }
-    }
-
-    [TestMethod]
-    // Test Scenario: Tim has infinite turns, and the others have finite turns.
-    // Expected result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    public void TestTakingTurnsQueue_ForeverZero()
-    {
-        var bob = new Person("Bob", 2);
-        var tim = new Person("Tim", 0);  // Infinite turns
-        var sue = new Person("Sue", 3);
-
-        Person[] expectedResult = new[] { bob, tim, sue, bob, tim, sue, tim, sue, tim, tim };
-
-        var players = new TakingTurnsQueue();
-        players.AddPerson(bob.Name, bob.Turns);
-        players.AddPerson(tim.Name, tim.Turns);
-        players.AddPerson(sue.Name, sue.Turns);
-
-        for (int i = 0; i < 10; i++)
-        {
-            Assert.AreEqual(expectedResult[i].Name, players.GetNextPerson().Name);
-        }
-    }
-
-    [TestMethod]
-    // Test Scenario: Handle empty queue.
-    [ExpectedException(typeof(InvalidOperationException))]
-    public void TestTakingTurnsQueue_Empty()
-    {
-        var players = new TakingTurnsQueue();
-        players.GetNextPerson(); // Should throw exception as queue is empty
+        Name = name;
+        Turns = turns;
     }
 }
