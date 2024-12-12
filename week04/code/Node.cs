d50ab912cdbaaa965fc238139029fc9321681ded
tmosimanyana <mos23018@byui.edu>
2024-12-13 01:53:39 +0200

    files updated

--- week04/code/Node.cs
+++ week04/code/Node.cs
@@ -1,1 +1,16 @@
-﻿
+﻿namespace LinkedListNamespace
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node? Next { get; set; }
+        public Node? Prev { get; set; }
+
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+    }
+}
+
+
