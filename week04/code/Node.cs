e873d4f1fd5d0e8e74ba244f3e0cb691de03c0f6
tmosimanyana <mos23018@byui.edu>
2024-12-13 01:49:06 +0200

    updated files

--- week04/code/Node.cs
+++ week04/code/Node.cs
@@ -1,12 +1,1 @@
-﻿// DO NOT MODIFY THIS FILE
-public class Node
-{
-    public int Data { get; set; }
-    public Node? Next { get; set; }
-    public Node? Prev { get; set; }
-
-    public Node(int data)
-    {
-        this.Data = data;
-    }
-}
+﻿
