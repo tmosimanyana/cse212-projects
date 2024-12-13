b6f8f29412ca22199b4400e4d31d94f7d7dbdfbe
tmosimanyana <mos23018@byui.edu>
2024-12-13 02:46:50 +0200

    files updated

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,13 +1,45 @@
-public class Node
+class DoublyLinkedList
 {
-    public int Data { get; set; }
-    public Node? Next { get; set; }
-    public Node? Prev { get; set; }
+    public Node? Head { get; set; }
+    public Node? Tail { get; set; }
 
-    public Node(int data)
+    public void AddNode(int data)
     {
-        this.Data = data;
-        this.Next = null;
-        this.Prev = null;
+        Node newNode = new Node(data);
+        if (Head == null)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            Tail!.Next = newNode; // Set the next of the current tail to the new node
+            newNode.Prev = Tail;   // Set the previous of the new node to the current tail
+            Tail = newNode;        // Update the tail to the new node
+        }
+    }
+
+    public void PrintList()
+    {
+        Node? current = Head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.AddNode(10);
+        list.AddNode(20);
+        list.AddNode(30);
+
+        list.PrintList(); // Output: 10 20 30
     }
 }
