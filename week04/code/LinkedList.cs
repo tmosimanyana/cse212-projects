e2ce713684d81adb8350cae3b302c7bdc8154700
tmosimanyana <mos23018@byui.edu>
2024-12-13 02:26:04 +0200

    files updated

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,43 +1,13 @@
-public class LinkedList
+public class Node
 {
-    public Node? Head { get; set; }
-    public Node? Tail { get; set; }
+    public int Data { get; set; }
+    public Node? Next { get; set; }
+    public Node? Prev { get; set; }
 
-    // Insert a node at the tail of the list
-    public void InsertTail(int data)
+    public Node(int data)
     {
-        // Create the new node
-        Node newNode = new Node(data);
-
-        // If the list is empty, set both Head and Tail to the new node
-        if (Head == null)
-        {
-            Head = newNode;
-            Tail = newNode;
-        }
-        else
-        {
-            // Otherwise, find the current tail and update it
-            Tail.Next = newNode;
-            newNode.Prev = Tail;
-            Tail = newNode;
-        }
-    }
-
-    // For debugging and testing purposes, override ToString method
-    public override string ToString()
-    {
-        Node? current = Head;
-        List<int> values = new List<int>();
-
-        while (current != null)
-        {
-            values.Add(current.Data);
-            current = current.Next;
-        }
-
-        return $"<LinkedList>{{{string.Join(", ", values)}}}";
+        this.Data = data;
+        this.Next = null;
+        this.Prev = null;
     }
 }
-
-
